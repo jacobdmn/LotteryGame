c046fb45559b54a5854cc8fb860ded5ff2f887a0
Jacob Dmn <jacob.demmane@email.com>
2022-01-14 23:38:52 +0300

    working on the test function

--- compile.js
+++ compile.js
@@ -24,3 +24,12 @@ const input = {
 module.exports = JSON.parse(solc.compile(JSON.stringify(input))).contracts[
   "Lottery.sol"
 ].Lottery;
+
+// const path = require("path"); //for crossplatform
+// const fs = require("fs"); //file system
+// const solc = require("solc");
+
+// const lotteryPath = path.resolve(__dirname, "contracts", "Lottery.sol");
+// const source = fs.readFileSync(lotteryPath, "utf8"); //change to utf8 to UTF-8
+
+// module.exports = solc.compile(source, 1).contracts[":Lottery"];
--- test/Lottery.test.js
+++ test/Lottery.test.js
@@ -1,22 +1,44 @@
 const assert = require("assert");
 const ganache = require("ganache-cli");
-const { abi, evm } = require("../compile");
 const Web3 = require("web3");
+const { abi, evm } = require("../compile");
+const HDWalletProvider = require("@truffle/hdwallet-provider");
 
-const web3 = new Web3(ganache.provider());
+// const web3 = new Web3(ganache.provider());
+
+const phrase =
+  "vessel female diet cycle memory trigger bike render stairs patient repeat vital";
+const endpoint =
+  "https://rinkeby.infura.io/v3/c95ed8d4ce414c50845b7ecc7dd4daa9";
+provider = new HDWalletProvider(phrase, endpoint);
+
+const web3 = new Web3(provider);
 
 let accounts, lottery;
 
-beforeEach(async () => {
-  accounts = web3.eth.getAccounts();
+// beforeEach(async () => {
+//   const accounts = await web3.eth.getAccounts();
 
-  lottery = await new web3.eth.Contract(abi)
+//   console.log("Current account", accounts[0]);
+
+//   const result = await new web3.eth.Contract(abi)
+//     .deploy({ data: evm.bytecode.object })
+//     .send({ gas: "10000000", from: accounts[0] });
+
+// });
+
+const beforeEachCustom = async () => {
+  const accounts = await web3.eth.getAccounts();
+
+  console.log("Current account", accounts[0]);
+
+  const result = await new web3.eth.Contract(abi)
     .deploy({ data: evm.bytecode.object })
     .send({ gas: "10000000", from: accounts[0] });
-});
-
+};
 describe("Lottery Contract", () => {
   it("deploys a contract", () => {
+    beforeEachCustom();
     assert.ok(lottery.options.address);
   });
 });
