32db026cec74a735f2d374ca0e5249cee97d6666
Jacob Dmn <jacob.demmane@email.com>
2022-01-14 06:43:44 +0300

    testtt

---
+++ test/Lottery.test.js
@@ -0,0 +1,24 @@
+const assert = require("assert");
+const ganache = require("ganache-cli");
+const { abi, evm } = require("../compile");
+const Web3 = require("web3");
+
+const web3 = new Web3(ganache.provider());
+const web4 = new Web3(ganache.provider());
+const web5 = new Web3(ganache.provider());
+
+let accounts, lottery;
+
+beforeEach(async () => {
+  accounts = web3.eth.getAccounts();
+
+  lottery = await new web3.eth.Contract(abi)
+    .deploy({ data: evm.bytecode.object })
+    .send({ gas: "10000000", from: accounts[0] });
+});
+
+describe("Lottery Contract", () => {
+  it("deploys a contract", () => {
+    assert.ok(lottery.options.address);
+  });
+});
